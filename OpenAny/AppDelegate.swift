09f7d88c257cf77930a161bb7063aed07952b54c
Christian Tietze <me@christiantietze.de>
2023-12-11 16:24:46 +0100

    hold on to URL scheme handler instance

--- OpenAny/AppDelegate.swift
+++ OpenAny/AppDelegate.swift
@@ -17,35 +17,35 @@ extension NSWorkspace.OpenConfiguration {
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     @IBOutlet var window: NSWindow!
 
+    lazy var urlSchemeHandler = URLSchemeHandler(actionHandler: { action in
+        switch action.lowercased(includingObject: false).moduleSubjectVerbObject() {
+        case (.file, "open", _, _):
+            guard let payload = action.payload else {
+                return
+            }
+
+            if let fileURL = payload["url"]?.flatMap(URL.init(string:)) {
+                NSWorkspace.shared.open(fileURL)
+            } else if let maybePath = payload["path"],
+                      let path = maybePath {
+                let fileURL = URL(fileURLWithPath: path)
+                NSWorkspace.shared.open(fileURL)
+            }
+
+        case (.app, let appBundleIdentifier, "launch", _):
+            guard let appURL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: appBundleIdentifier)
+            else { return }
+            NSWorkspace.shared.openApplication(
+                at: appURL,
+                configuration: .init(fromPayload: action.payload))
+
+        default: return
+        }
+    })
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        URLSchemeHandler(actionHandler: { action in
-            switch action.lowercased(includingObject: false).moduleSubjectVerbObject() {
-            case (.file, "open", _, _):
-                guard let payload = action.payload else {
-                    return
-                }
-
-                if let fileURL = payload["url"]?.flatMap(URL.init(string:)) {
-                    NSWorkspace.shared.open(fileURL)
-                } else if let maybePath = payload["path"],
-                          let path = maybePath {
-                    let fileURL = URL(fileURLWithPath: path)
-                    NSWorkspace.shared.open(fileURL)
-                }
-
-            case (.app, let appBundleIdentifier, "launch", _):
-                guard let appURL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: appBundleIdentifier)
-                else { return }
-                NSWorkspace.shared.openApplication(
-                    at: appURL,
-                    configuration: .init(fromPayload: action.payload))
-
-            default: return
-            }
-        }).install()
+        urlSchemeHandler.install()
     }
 }
