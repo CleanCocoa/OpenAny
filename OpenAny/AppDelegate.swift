25bbcf3a033639c26c791006b340afca7ee8ee52
Christian Tietze <me@christiantietze.de>
2023-12-11 17:56:13 +0100

    expand ~ in path

--- OpenAny/AppDelegate.swift
+++ OpenAny/AppDelegate.swift
@@ -20,7 +20,7 @@ extension Payload {
         if let fileURL = self["url"]?.flatMap(URL.init(string:)) {
             return fileURL
         } else if let maybePath = self["path"],
-                  let path = maybePath {
+                  let path = (maybePath as NSString?)?.expandingTildeInPath {
             return URL(fileURLWithPath: path)
         }
         return nil
