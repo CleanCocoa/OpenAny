bc488fc2249eb1598c9672a6746974d6bb8c1952
Christian Tietze <me@christiantietze.de>
2023-12-11 15:13:11 +0100

    parse ://file?url= and ://file?path= actions

--- OpenAny/AppDelegate.swift
+++ OpenAny/AppDelegate.swift
@@ -3,6 +3,7 @@ import URLSchemer
 
 extension Module {
     static var app = Module("app")
+    static var file = Module("file")
 }
 
 extension NSWorkspace.OpenConfiguration {
@@ -23,6 +24,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         URLSchemeHandler(actionHandler: { action in
             switch action.lowercased(includingObject: false).moduleSubjectVerbObject() {
+            case (.file, "open", _, _):
+                guard let payload = action.payload else {
+                    return
+                }
+
+                if let fileURL = payload["url"]?.flatMap(URL.init(string:)) {
+                    NSWorkspace.shared.open(fileURL)
+                } else if let maybePath = payload["path"],
+                          let path = maybePath {
+                    let fileURL = URL(fileURLWithPath: path)
+                    NSWorkspace.shared.open(fileURL)
+                }
+
             case (.app, let appBundleIdentifier, "launch", _):
                 guard let appURL = NSWorkspace.shared.urlForApplication(withBundleIdentifier: appBundleIdentifier)
                 else { return }
